75105875d15de1249e52ebee06df95b4ea3d038b
OneTrueKyle <madding.kyle@proton.me>
2024-12-16 13:37:42 +0200

    removed interrupts (won't be needed) using a test program from armasm.org

--- run.py
+++ run.py
@@ -4,18 +4,10 @@ import sys
 
 # Memory configuration
 BASE_ADDR = 0x1000
+
+IN_BUF = 0x1020
+OUT_BUF = 0X1040
 MEM_SIZE = 2 * 1024 * 1024  # 2MB
-
-
-# callback for tracing basic blocks
-def hook_block(uc, address, size, user_data):
-    print(f">>> Tracing basic block at {hex(address)}, block size = {hex(size)}")
-
-
-# callback for tracing instructions
-def hook_code(uc, address, size, user_data):
-    print(f">>> Tracing instruction at {hex(address)}, instruction size = {hex(size)}")
-
 
 def load_binary(file_path):
     try:
@@ -40,21 +32,13 @@ def emulate_arm(binary_code):
         # Set the program counter (PC) to the start address
         uc.reg_write(UC_ARM_REG_PC, BASE_ADDR)
 
-        # Tracing all basic blocks with customized callback
-        uc.hook_add(UC_HOOK_CODE, hook_code)
-
         # Start emulation
         uc.emu_start(BASE_ADDR, BASE_ADDR + len(binary_code))
 
-        # Print registers
-        print(">>> Emulation done. Below is the CPU context")
+        # Print results
+        print(">>> Emulation done.")
 
-        r0 = uc.reg_read(UC_ARM_REG_R0)
-        r1 = uc.reg_read(UC_ARM_REG_R1)
-        r2 = uc.reg_read(UC_ARM_REG_R2)
-        print(f">>> R0 = {hex(r0)} ({r0})")
-        print(f">>> R1 = {hex(r1)} ({r1})")
-        print(f">>> R2 = {hex(r2)} ({r2})")
+        # TODO print out + in memory
 
 
     except UcError as e:
